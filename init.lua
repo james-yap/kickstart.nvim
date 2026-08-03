60e14cbf35068bc0ed5b7388e8b68c35c7e6d48e
James Yap <contact@jamesyap.org>
2026-08-03 16:24:55 -0400

    feat: display total lines and percentage in status bar

--- init.lua
+++ init.lua
@@ -443,7 +443,7 @@ do
   -- default behavior. For example, here we set the section for
   -- cursor location to LINE:COLUMN
   ---@diagnostic disable-next-line: duplicate-set-field
-  statusline.section_location = function() return '%2l:%-2v' end
+  statusline.section_location = function() return '%2l/%L:%-2v | %p%%' end
 
   -- ... and there is more!
   --  Check out: https://github.com/nvim-mini/mini.nvim
