3abfa18374cd90f31359e0b985dc249d1e29ef4a
James Yap <contact@jamesyap.org>
2026-08-05 23:59:54 -0400

    feat: colorscheme to quiet

--- init.lua
+++ init.lua
@@ -384,17 +384,17 @@ do
   -- change the command under that to load whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  vim.pack.add { gh 'rose-pine/neovim' }
-  require('rose-pine').setup {
-    styles = {
-      comments = { italic = false },
-    },
-  }
+  -- vim.pack.add { gh 'rose-pine/neovim' }
+  -- require('rose-pine').setup {
+  --   styles = {
+  --     comments = { italic = false },
+  --   },
+  -- }
 
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'rose-pine'
+  vim.cmd.colorscheme 'quiet'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
