25247ce29daea9544b9263d6f6cd73b0f07d8fea
James Yap <contact@jamesyap.org>
2026-08-09 13:20:22 -0400

    feat: added Octo.nvim

---
+++ lua/custom/plugins/octo.lua
@@ -0,0 +1,16 @@
+vim.pack.add {
+  { src = 'https://github.com/pwntester/octo.nvim' },
+  { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
+}
+
+require('octo').setup {
+  picker = 'telescope', -- or "fzf-lua" or "snacks" or "default"
+  enable_builtin = true,
+}
+
+vim.keymap.set('n', '<leader>oi', '<CMD>Octo issue list<CR>', { desc = 'List GitHub Issues' })
+vim.keymap.set('n', '<leader>op', '<CMD>Octo pr list<CR>', { desc = 'List GitHub PullRequests' })
+vim.keymap.set('n', '<leader>od', '<CMD>Octo discussion list<CR>', { desc = 'List GitHub Discussions' })
+vim.keymap.set('n', '<leader>on', '<CMD>Octo notification list<CR>', { desc = 'List GitHub Notifications' })
+
+vim.keymap.set('n', '<leader>os', function() require('octo.utils').create_base_search_command { include_current_repo = true } end, { desc = 'Search GitHub' })
