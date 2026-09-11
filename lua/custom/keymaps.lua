31903dc4b3c7998b061d299b4ed8bb088866da5f
James Yap <contact@jamesyap.org>
2026-09-10 22:03:09 -0400

    feat: render latex keymap

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -38,3 +38,29 @@ vim.keymap.set('n', 'g-', function()
 
   vim.notify(string.format( 'squashed "%s" to previous jj commit' , filename), vim.log.levels.INFO)
 end, { desc = 'Squash current file to previous jj commit' })
+
+vim.keymap.set('n', '<leader>rl', function()
+  local abspath = vim.api.nvim_buf_get_name(0)
+  if vim.fn.fnamemodify(abspath, ':e') ~= 'tex' then
+    vim.notify('current file is not .tex', vim.log.levels.WARN)
+    return
+  end
+
+  local filename = vim.fn.fnamemodify(abspath, ':t')
+  local dir = vim.fn.fnamemodify(abspath, ':h')
+  local result = vim.system(
+    { 'pdflatex', '-interaction=nonstopmode', filename },
+    { cwd = dir, text = true }
+  ):wait()
+
+  if result.code ~= 0 then
+    local output = vim.trim((result.stdout or '') .. (result.stderr or ''))
+    local lines = vim.split(output, '\n', { plain = true })
+    local start = math.max(1, #lines - 19)
+    local err_msg = table.concat(vim.list_slice(lines, start, #lines), '\n')
+    vim.notify(string.format('pdflatex failed:\n%s', err_msg), vim.log.levels.ERROR)
+    return
+  end
+
+  vim.notify(string.format('compiled "%s"', filename), vim.log.levels.INFO)
+end, { desc = 'Run pdflatex on current file' })
